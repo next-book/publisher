38e459a1aac1b09f17c6422fc86e71e413729a3a
cernockyd <24832873+cernockyd@users.noreply.github.com>
2021-08-18 11:11:35 +0200

    ts-jest modules

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
+
+export default {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
+  testPathIgnorePatterns: ["dist"],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  },
 };
