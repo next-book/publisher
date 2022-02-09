63b7b8fec0335255ab1807cceac9d5db3733c820
cernockyd <24832873+cernockyd@users.noreply.github.com>
2022-02-09 14:05:42 +0100

    simplify navigation and roles addition for unit tests

--- src/chapter-navigation.ts
+++ src/chapter-navigation.ts
@@ -6,34 +6,32 @@ import { Id, Rel, PageClass, URLFragment, NavClass, ChapterId } from '../shared/
  * Adds navigation between chapters (prev chapter link at the beggining and
  * next chapter link at the end) to a provided list of documents (chapters).
  *
- * @param chapters - Array of Documents each representing a chapter
+ * @param doc - Document representing a chapter
  * @param root - DOMString for selecting content root
  */
-export function addChapterInPageNavigation(chapters: Document[], root: DOMStringLike): void {
-  chapters.forEach(doc => {
-    const content = doc.querySelector(root);
-    const self = doc.querySelector<HTMLLinkElement>('link[rel="self"]')?.getAttribute('href');
-    if (!content) return;
+export function addChapterInPageNavigation(doc: Document, root: DOMStringLike): void {
+  const content = doc.querySelector(root);
+  const self = doc.querySelector<HTMLLinkElement>('link[rel="self"]')?.getAttribute('href');
+  if (!content) return;
 
-    const beginNav = createNavFragment(doc, NavClass.Begin);
-    const endNav = createNavFragment(doc, NavClass.End);
+  const beginNav = createNavFragment(doc, NavClass.Begin);
+  const endNav = createNavFragment(doc, NavClass.End);
 
-    createLink(doc, Rel.Index, '', t('navigation:title-page'), anchor => {
-      appendLinkToNav(doc, beginNav, anchor.cloneNode(true));
-      appendLinkToNav(doc, endNav, anchor.cloneNode(true));
-    });
-
-    createLink(doc, Rel.Prev, ChapterId.End, `← ${t('navigation:prev-chapter')}`, anchor => {
-      appendLinkToNav(doc, beginNav, anchor);
-    });
-
-    createLink(doc, Rel.Next, 'chunk1', `${t('navigation:next-chapter')} →`, anchor => {
-      appendLinkToNav(doc, endNav, anchor);
-    });
-
-    if (self !== 'index.html') content.insertBefore(beginNav, content.firstChild);
-    content.appendChild(endNav);
+  createLink(doc, Rel.Index, '', t('navigation:title-page'), anchor => {
+    appendLinkToNav(doc, beginNav, anchor.cloneNode(true));
+    appendLinkToNav(doc, endNav, anchor.cloneNode(true));
   });
+
+  createLink(doc, Rel.Prev, ChapterId.End, `← ${t('navigation:prev-chapter')}`, anchor => {
+    appendLinkToNav(doc, beginNav, anchor);
+  });
+
+  createLink(doc, Rel.Next, 'chunk1', `${t('navigation:next-chapter')} →`, anchor => {
+    appendLinkToNav(doc, endNav, anchor);
+  });
+
+  if (self !== 'index.html') content.insertBefore(beginNav, content.firstChild);
+  content.appendChild(endNav);
 }
 
 function createNavFragment(doc: Document, className: NavClass): DocumentFragment {
@@ -81,47 +79,41 @@ function createLink(
   }
 }
 
-export function addChapterStartAnchor(documents: Document[], root: DOMStringLike): void {
-  documents.forEach(doc => {
-    const content = doc.querySelector(root);
-    if (!content) return;
+export function addChapterStartAnchor(doc: Document, root: DOMStringLike): void {
+  const content = doc.querySelector(root);
+  if (!content) return;
 
-    const anchor = doc.createElement('a');
-    anchor.setAttribute('id', ChapterId.Start);
+  const anchor = doc.createElement('a');
+  anchor.setAttribute('id', ChapterId.Start);
 
-    doc.querySelector(root)?.insertBefore(anchor, content.firstChild);
-  });
+  doc.querySelector(root)?.insertBefore(anchor, content.firstChild);
 }
 
-export function addChapterEndAnchor(documents: Document[], root: DOMStringLike): void {
-  documents.forEach(doc => {
-    const content = doc.querySelector(root);
-    if (!content) return;
+export function addChapterEndAnchor(doc: Document, root: DOMStringLike): void {
+  const content = doc.querySelector(root);
+  if (!content) return;
 
-    const anchor = doc.createElement('a');
-    anchor.setAttribute('id', ChapterId.End);
+  const anchor = doc.createElement('a');
+  anchor.setAttribute('id', ChapterId.End);
 
-    content.appendChild(anchor);
-  });
+  content.appendChild(anchor);
 }
 
-export function addFullTextUrl(documents: Document[], url: string, root: DOMStringLike): void {
-  documents.forEach(doc => {
-    const content = doc.querySelector(root);
-    if (!content) return;
+export function addFullTextUrl(doc: Document, url: string, root: DOMStringLike): void {
+  const content = doc.querySelector(root);
+  if (!content) return;
 
-    const p = doc.createElement('p');
-    p.setAttribute('id', Id.FullTextLink);
+  const p = doc.createElement('p');
+  p.setAttribute('id', Id.FullTextLink);
 
-    const anchor = doc.createElement('a');
-    anchor.setAttribute('href', url);
-    anchor.innerHTML = `Toto je ukázka. Pro zobrazení plné verze knihy následujte tento odkaz.`; // todo: translate
-    p.appendChild(anchor);
+  const anchor = doc.createElement('a');
+  anchor.setAttribute('href', url);
+  anchor.innerHTML = `Toto je ukázka. Pro zobrazení plné verze knihy následujte tento odkaz.`; // todo: translate
+  p.appendChild(anchor);
 
-    if (doc.querySelector('body')?.getAttribute('class') === PageClass.Home) {
-      content.insertBefore(p, content.firstChild);
-    } else {
-      content.appendChild(p);
-    }
-  });
+  if (doc.querySelector('body')?.getAttribute('class') === PageClass.Home) {
+    content.insertBefore(p, content.firstChild);
+  } else {
+    content.appendChild(p);
+  }
 }
