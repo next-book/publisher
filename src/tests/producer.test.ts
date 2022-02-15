0c509eb8b441a35b5fa976f13f697c92c7f61421
cernockyd <24832873+cernockyd@users.noreply.github.com>
2022-02-15 11:35:19 +0100

    test producer produceHTMLSpanIdea

--- src/tests/producer.test.ts
+++ src/tests/producer.test.ts
@@ -3,7 +3,8 @@
  */
 
 import { Separator } from '../parser';
-import produce, { anchorObject, containsParsedObj } from '../producer';
+import produce, { anchorObject, containsParsedObj, produceHTMLSpanIdea } from '../producer';
+import { IdeaPiece } from '../structures';
 import ParsedObj from '../structures/parsedobj';
 
 describe('produce', () => {
@@ -544,8 +545,87 @@ describe('containsParsedObj', () => {
 });
 
 describe('produceHTMLSpanIdea', () => {
-  it.todo('throw error when provided wront input (parsedObj)');
-  it.todo('should produce span');
+  it('should return span idea with no children when provided empty array', () => {
+    const ideaPieces: IdeaPiece[] = [new ParsedObj(document.createElement('main'), [], '\n')];
+
+    expect(() => produceHTMLSpanIdea(ideaPieces, document)).toThrowError(
+      'Idea children contain ParsedObj.'
+    );
+  });
+
+  it('should return span idea with no children when provided empty array', () => {
+    const ideaPieces: IdeaPiece[] = [];
+
+    const expected = document.createElement('span');
+    expected.classList.add('idea');
+
+    expect(produceHTMLSpanIdea(ideaPieces, document)).toStrictEqual(expected);
+  });
+
+  it('should return span idea with no children when provided array whose only item is Separator', () => {
+    const ideaPieces: IdeaPiece[] = [new Separator()];
+
+    const expected = document.createElement('span');
+    expected.classList.add('idea');
+
+    expect(produceHTMLSpanIdea(ideaPieces, document)).toStrictEqual(expected);
+  });
+
+  it('should return span idea when provided simple string children', () => {
+    const ideaPieces = ['a', 'b', 'c'];
+
+    const expected = document.createElement('span');
+    expected.classList.add('idea');
+    expected.appendChild(document.createTextNode('a'));
+    expected.appendChild(document.createTextNode('b'));
+    expected.appendChild(document.createTextNode('c'));
+
+    expect(produceHTMLSpanIdea(ideaPieces, document)).toStrictEqual(expected);
+  });
+
+  it('should return span idea containing text node children, when provided strings and Separator', () => {
+    const ideaPieces = ['a', 'b', new Separator(), 'c'];
+
+    const expected = document.createElement('span');
+    expected.classList.add('idea');
+    expected.appendChild(document.createTextNode('a'));
+    expected.appendChild(document.createTextNode('b'));
+    expected.appendChild(document.createTextNode('c'));
+
+    expect(produceHTMLSpanIdea(ideaPieces, document)).toStrictEqual(expected);
+  });
+
+  it('should return span idea containing anchor when provided list containing anchor', () => {
+    const link = document.createElement('a');
+    link.appendChild(document.createTextNode('b'));
+    const ideaPieces = ['a', link, 'c'];
+
+    const expected = document.createElement('span');
+    expected.classList.add('idea');
+    expected.appendChild(document.createTextNode('a'));
+    const anchor = document.createElement('a');
+    anchor.appendChild(document.createTextNode('b'));
+    expected.appendChild(anchor);
+    expected.appendChild(document.createTextNode('c'));
+
+    expect(produceHTMLSpanIdea(ideaPieces, document)).toStrictEqual(expected);
+  });
+
+  it('should return span idea and ignore separator', () => {
+    const link = document.createElement('a');
+    link.appendChild(document.createTextNode('b'));
+    const ideaPieces = ['a', link, new Separator(), 'c'];
+
+    const expected = document.createElement('span');
+    expected.classList.add('idea');
+    expected.appendChild(document.createTextNode('a'));
+    const anchor = document.createElement('a');
+    anchor.appendChild(document.createTextNode('b'));
+    expected.appendChild(anchor);
+    expected.appendChild(document.createTextNode('c'));
+
+    expect(produceHTMLSpanIdea(ideaPieces, document)).toStrictEqual(expected);
+  });
 });
 
 describe('anchorObject', () => {
