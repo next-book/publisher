e19796b8d5e9066aa1dbd5ad9b036ba48fee8fdb
cernockyd <24832873+cernockyd@users.noreply.github.com>
2022-07-06 16:52:38 +0200

    util for dom tree debugging

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -10,3 +10,28 @@ export function dumpArray(arr: unknown) {
     .map(line => `>${line}`)
     .join('\n');
 }
+
+/**
+ * Returns string tree representation of Node and its children, useful for logging
+ *
+ * @param node root Node
+ * @param index order number of child
+ * @param level depth of recursion used for tree indentation
+ * @returns
+ */
+export function dumpNode(node: Node, index = 0, level = 0) {
+  if (!node) {
+    return;
+  }
+  let result = '';
+  result += '  '.repeat(level) + index + '. ';
+  if (node.nodeType == Node.ELEMENT_NODE) {
+    result += `element: ${node.nodeName}`;
+  }
+  if (node.nodeType == Node.TEXT_NODE) {
+    result += `textNode: \'${node.textContent?.replace('\n', '\\n')}\'`;
+  }
+  result += '\n';
+  node.childNodes.forEach((ch, i) => (result += dumpNode(ch, i, level + 1)));
+  return result;
+}
